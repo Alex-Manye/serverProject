4221f34ea781f8fa2b250069a88388d5e0e5c279
Daniel <wbmedia@gmail.com>
2019-11-10 23:29:41 +0100

    first commit

---
+++ routes/project-routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+const Project = require("../models/Project");
+const Task = require("../models/Task");
+
+// GET route => get all projects
+
+// POST route => to create a new project
+
+
+// GET route => to get a specific project/detailed view
+
+
+// PUT route => to update a specific project
+
+// DELETE route => to delete a specific project
+
+module.exports = router;
